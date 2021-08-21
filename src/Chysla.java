f3d22818857ce0d85683bb3a84b76a36773aa77f
MarinaAleksandrovna <89290750+MarinaAleksandrovna@users.noreply.github.com>
2021-08-21 20:24:58 +0300

    Update Chysla.java

--- src/Chysla.java
+++ src/Chysla.java
@@ -8,7 +8,7 @@ public class Chysla {
 			case '/': rez=chys1/chys2; break;
 			case '+': rez=chys1+chys2; break;
 			case '-': rez=chys1-chys2; break;
-			default: System.out.println("Не корректное действие");
+			default: System.out.println("Ne korrektniy vvod");
 			}
 			 return rez;
 		 }
